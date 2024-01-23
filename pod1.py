2d1069d1b86b481d161d848bcfea724edb05bf37
Rajkumar Ramoliya <82795154+rramoliya005@users.noreply.github.com>
2024-01-23 12:14:31 -0700

    Add files via upload

---
+++ pod1.py
@@ -0,0 +1,13 @@
+class CheckingAcc:
+    #class body
+    pass
+class SavingAcc:
+    #class body
+    pass
+class Employee:
+    def __init__(self,name,age)
+        self.name = name
+        self.age
+
+Employee = Employee
+        
